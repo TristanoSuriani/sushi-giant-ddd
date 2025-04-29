644b579fa1fefdbae51d4657200962b747febd35
suriani <tristano.suriani@ordina.nl>
2025-04-29 22:41:59 +0200

    initial commit

---
+++ src/main/java/nl/suriani/sushi_giant_ddd/SushiGiantDddWorkshopApplication.java
@@ -0,0 +1,13 @@
+package nl.suriani.sushi_giant_ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiGiantDddWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushiGiantDddWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/suriani/sushi_giant_ddd/SushiGiantDddWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.suriani.sushi_giant_ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiGiantDddWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
